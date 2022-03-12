4239dfae8b0189fc0f2870e23195fdb7743a5d66
Jconradop1412 <97989172+Jconradop1412@users.noreply.github.com>
2022-03-12 17:05:09 -0500

    Modified app.js and movie.js

--- src/App.js
+++ src/App.js
@@ -78,7 +78,65 @@ function App() {
 
     return (
         <div className="App">
-            <h1>Hello World</h1>
+            <header className="center-max-size header">
+                <span className={"brand"}>Movie Trailer App</span>
+                <form className="form" onSubmit={fetchMovies}>
+                    <input className="search" type="text" id="search"
+                           onInput={(event) => setSearchKey(event.target.value)}/>
+                    <button className="submit-search" type="submit"><i className="fa fa-search"></i></button>
+                </form>
+            </header>
+            {movies.length ?
+                <main>
+                    {movie ?
+                        <div className="poster"
+                             style={{backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(${BACKDROP_PATH}${movie.backdrop_path})`}}>
+                            {playing ?
+                                <>
+                                    <Youtube
+                                        videoId={trailer.key}
+                                        className={"youtube amru"}
+                                        containerClassName={"youtube-container amru"}
+                                        opts={
+                                            {
+                                                width: '100%',
+                                                height: '100%',
+                                                playerVars: {
+                                                    autoplay: 1,
+                                                    controls: 0,
+                                                    cc_load_policy: 0,
+                                                    fs: 0,
+                                                    iv_load_policy: 0,
+                                                    modestbranding: 0,
+                                                    rel: 0,
+                                                    showinfo: 0,
+                                                },
+                                            }
+                                        }
+                                    />
+                                    <button onClick={() => setPlaying(false)} className={"button close-video"}>Close
+                                    </button>
+                                </> :
+                                <div className="center-max-size">
+                                    <div className="poster-content">
+                                        {trailer ?
+                                            <button className={"button play-video"} onClick={() => setPlaying(true)}
+                                                    type="button">Play
+                                                Trailer</button>
+                                            : 'Sorry, no trailer available'}
+                                        <h1>{movie.title}</h1>
+                                        <p>{movie.overview}</p>
+                                    </div>
+                                </div>
+                            }
+                        </div>
+                        : null}
+
+                    <div className={"center-max-size container"}>
+                        {renderMovies()}
+                    </div>
+                </main>
+                : 'Sorry, no movies found'}
         </div>
     );
 }
